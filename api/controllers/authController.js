dccefc0a22ff8ee6f4e4cf385866d3aa00d0385b
Manglesh <mangleshyadav5456@gmail.com>
2024-03-16 14:07:41 +0530

    Added middleware to handle Error

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,7 +1,7 @@
 const User = require("../models/userModel");
 const bcryptjs = require("bcryptjs");
 
-module.exports.signup = async (req, res) => {
+module.exports.signup = async (req, res, next) => {
     //console.log(req.body);
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password,10);
@@ -10,7 +10,7 @@ module.exports.signup = async (req, res) => {
         await newUser.save();
         res.status(201).json({ message: "User created successfully" });
     } catch(error) {
-        res.status(500).json(error.message);
+        next(error);
     }
     
 }
--- api/index.js
+++ api/index.js
@@ -21,3 +21,13 @@ app.listen(3000,() => {
 
 app.use('/api/user',userRoutes);
 app.use('/api/auth',authRoutes);
+
+app.use((error,req,res) => {
+    const statusCode = error.statusCode || 500;
+    const message = error.message ||'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode
+    })
+})
